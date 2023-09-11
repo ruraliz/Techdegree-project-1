9b987b0eddb9808b2db825fb09505c97150e6865
Liz Rurangwa <107972391+ruraliz@users.noreply.github.com>
2023-09-11 11:49:41 -0700

    added if statements to object keys for when a key is undefined

--- js/script.js
+++ js/script.js
@@ -15,14 +15,13 @@ const quotes = [   //created a variable named quotes with an array of objects co
     quote:"For me, I'm a dancer first. I could be the President of the United States, and I will always be a dancer, first and foremost.", 
     source:'Anne Fletcher', 
     citation:'', 
-    year:''
+  
   },
   {
     quote:"I don't want a childhood, I want to be a ballet dancer.", 
     source:'Jamie Bell', 
     citation:'Billy Elliot', 
-    year:'',
-    tags: 'satire'
+    tags: 'Coming of Age'
   },
   {
     quote:"There’s no better feeling. The truth is you only fly for a second or two before gravity takes back what you tried to steal. Because no matter how hard you work, how strong you are, or how thin you get, gravity always, always wins.", 
@@ -35,20 +34,19 @@ const quotes = [   //created a variable named quotes with an array of objects co
     quote:"I used to know a whole lot of dances. The-the-the-eh- the flea hop, and-and, what's the - eh - the black bottom, the variety drag. I don't, I don't know, I just don't seem to have any time any more. I have so many things to do.", 
     source:'James Stewart', 
     citation:'Harvey', 
-    year:'1950'
+    year:'1950',
   },
   {
     quote:"When you dance the dance of another, you make yourself in the image of its creator. You empty yourself so that her work can live within you.", 
     source:'Tilda Swinton', 
     citation:'Suspiria', 
-    year:'2018'
+    year:'2018',
   },
   {
     quote:"There are two types of beings in the universe, those who dance, and those who do not.", 
     source:'Dave Bautista', 
     citation:'Avengers', 
-    year:'',
-    tags: 'humor'
+    tags: 'Humor'
   }
 ];
 
@@ -84,11 +82,14 @@ function printQuote(){ // function to print the quotes on the web page
   const randomQuotes= getRandomQuote(quotes);
   let html = `<p class = "quote">` + randomQuotes.quote + '</p>'
   + `<p class = "source">` + randomQuotes.source 
-  if ( randomQuotes.citation !== null){
+  if ( randomQuotes.citation !== '' &&  randomQuotes.citation !== undefined){
     html += `<span class= "citation">` + randomQuotes.citation + `</span>`
   }
-  if ( randomQuotes.year !== null) {
+  if ( randomQuotes.year !== undefined) {
     html += `<span class= "year">` + randomQuotes.year + `</span>`
+  }
+  if ( randomQuotes.tags !== undefined) {
+    html += `<span class= "tags">` + randomQuotes.tags + `</span>`
   }
   document.getElementById('quote-box').innerHTML = html; // pulls format from index.html page and prints the quotes on the web page.
 }
